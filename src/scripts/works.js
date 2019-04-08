89ba232fd80989d2f521f1f07af9d0b6732eb22d
Evgeniy Sheleshkov <e.sheleshkov@gmail.com>
2019-04-08 17:45:57 +0300

    Виджит works продолжение

--- src/scripts/works.js
+++ src/scripts/works.js
@@ -1,45 +1,51 @@
 import Vue from "vue";
 
 const thumbs = {
-    template: "#slider-thumbs"
+  template: "#slider-thumbs",
+  props: {
+    works: Array //1:17:27
+  }
 };
 
 const btns = {
-    template: "#slider-btns"
+  template: "#slider-btns"
 };
 
 const display = {
-    template: "#slider-display",
-    components: {
-        btns, thumbs
-    }
+  template: "#slider-display",
+  components: {
+    btns, thumbs
+  },
+  props: {
+    works: Array
+  }
 };
 
 const tags = {
-    template: "#slider-tags"
+  template: "#slider-tags"
 };
 
 const info = {
-    template: "#slider-info",
-    components: {
-        tags
-    }
+  template: "#slider-info",
+  omponents: {
+    tags
+  }
 };
 
 new Vue ({
-    template: "#slider-container",
-    el: "#slider-component",
-    components: {
-        display,
-        info
-    },
-    data() {
-        return {
-            works: []
-        }
-    },
-    created () {
-        const data = require("../data/works.json");
-        this.works = data;
+  template: "#slider-container",
+  el: "#slider-component",
+  components: {
+    display,
+    info
+  },
+  data() {
+    return {
+      works: []
     }
+  },
+  created () {
+    const data = require("../data/works.json");
+    this.works = data;
+  }
 });
